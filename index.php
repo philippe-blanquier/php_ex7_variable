a976af50ca5b6f6c54a0e2d157c42f88cf90a71b
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 11:04:41 +0200

    creation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex7</title>
+    </head>
+    <body >
+        <?php
+        $lastname="Last";
+        $firstname="First";
+        $age=18;
+        echo nl2br("Exercise 7\n".$lastname." ".$firstname.", tu as ".$age." ans");
+        ?>
+    </body>
+</html>
