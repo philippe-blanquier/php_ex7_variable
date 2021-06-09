7964151d5ed04f0d6c56cfd31859979de76aecc6
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:23:18 +0200

    evo

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang='fr'>
 	<head>
-        <title>PHP ex7</title>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 1.7</title>
     </head>
     <body >
-        <?php
-        $lastname="Last";
-        $firstname="First";
-        $age=18;
-        echo nl2br("Exercise 7\n".$lastname." ".$firstname.", tu as ".$age." ans");
-        ?>
+        <p>
+            <?php
+            $lastname='Last';
+            $firstname='First';
+            $age=18;
+            echo nl2br("Exercise 1.7\n".$lastname.' '.$firstname.', tu as '.$age.' ans');
+            ?>
+        </p>
     </body>
 </html>
